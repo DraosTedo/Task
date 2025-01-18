9ed61b3e5e9de04349bbc2db77e91199db2f1472
Teodor <teodorkostadinov@abv.bg>
2025-01-18 11:16:13 +0200

    Spliting Terraform configuration onto separete files

---
+++ terraform/load_balancer.tf
@@ -0,0 +1,27 @@
+resource "aws_elb" "example" {
+  name            = "example"
+  subnets         = [aws_subnet.example.id]
+  security_groups = [aws_security_group.example.id]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  listener {
+    instance_port     = 8080
+    instance_protocol = "http"
+    lb_port           = 8080
+    lb_protocol       = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:80/"
+    interval            = 30
+  }
+}
